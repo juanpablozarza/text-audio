2b48480f5b68ee76fb8aae9b296c6671d8e0d36f
JP <jpzarza@jpzarza.local>
2023-10-11 10:36:51 -0600

    Testing new text-speech multilingual

--- index.py
+++ index.py
@@ -12,6 +12,7 @@ from datasets import load_dataset
 import io
 from scipy.io.wavfile import write
 from datetime import datetime
+import json
 mysp= __import__("my-voice-analysis")
 
 app = Flask(__name__)
@@ -102,7 +103,8 @@ def audioEval():
    
 @app.route("/bark/<uid>", methods=['POST'])
 def bark(uid):
-     data = request.data
+     data = json.loads(request.data)
+     
      text =data['textData']
      inputs = bark_preprocess(
      text=[text],
