c4355d30b5cbe102d11e116fa2e072fefe175ef1
JP <jpzarza@jpzarza.local>
2023-10-17 17:03:58 -0600

    multilingual test

--- index.py
+++ index.py
@@ -117,12 +117,8 @@ def spanishTTS(textData):
     inputs = spaTTSTokenizer(textData, return_tensors="pt")
     with torch.no_grad():
       output = spaTTS(**inputs).waveform()
-   
-    output_np = output.numpy()
-    print(output_np)
-    audio_data = np.int16(output_np/np.max(np.abs(output_np)) * 32767)
-  
-    write("results/output.wav", rate=spaTTS.config.sampling_rate, data=audio_data)
+    print(output)
+    write("results/output.wav", rate=spaTTS.config.sampling_rate, data=output)
     return output
 
 def textClassifier(textData):
