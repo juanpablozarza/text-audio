b92971f727615d1cf90f7db14519b4732eaeacea
juanpablozarza <85136464+juanpablozarza@users.noreply.github.com>
2023-11-07 23:45:11 -0600

    Update index.py

--- index.py
+++ index.py
@@ -125,7 +125,7 @@ def spanishTTS(textData):
     write("results/output.wav", rate=SAMPLE_RATE, data=audio_array)
     return audio_array
 def textClassifier(textData):
-    inputs = lang_sep_tokenizer(f"{textData} ->:", return_tensors='pt')
+    inputs = lang_sep_tokenizer(f"{textData} ->: [ ", return_tensors='pt')
     predictions = lang_sep_model.generate(**inputs, max_new_tokens=150)
     pred = lang_sep_tokenizer.decode(predictions[0], skip_special_tokens=True)
     print(pred)
