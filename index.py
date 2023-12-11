3e8c897a1cbdc36bdf38f89ddc17a8cc333f1566
juanpablozarza <85136464+juanpablozarza@users.noreply.github.com>
2023-12-11 09:08:39 -0600

    Update index.py

--- index.py
+++ index.py
@@ -21,7 +21,6 @@ from peft import PeftModel, PeftConfig
 from werkzeug.utils import secure_filename
 preload_models()
 from optimum.bettertransformer import BetterTransformer
-import scipy
 mysp= __import__("my-voice-analysis")
 # tts = CS_API()
 app = Flask(__name__)
@@ -65,10 +64,6 @@ textClassfierModel = AutoModelForSequenceClassification.from_pretrained(textClas
 text_classifier = TextClassificationPipeline(model=textClassfierModel, tokenizer=textClassfierTokenizer) 
 
 
-
-
-model_spa = VitsModel.from_pretrained("facebook/mms-tts-spa")
-tokenizer_spa = AutoTokenizer.from_pretrained("facebook/mms-tts-spa")
 
 
 kinesis = boto3.client("kinesis", region_name="us-west-1")
@@ -130,10 +125,6 @@ def generateAudioFile(uid):
     print('Generating audio file...')
     reqData = request.json
     textData = reqData.get("textData")
-    inputs = tokenizer_spa(textData, return_tensors="pt")
-    with torch.no_grad():
-      output = model_spa(**inputs).waveform
-      scipy.io.wavfile.write("techno.wav", rate=model_spa.config.sampling_rate, data=output)
     inputs = processor(text=textData, return_tensors="pt")
     embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
     speaker_embeddings = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
