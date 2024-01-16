ee4aa22ba11fa8da6af3bd8f4c4ac1ec70066c81
JP Zarza <jpzarza@JPs-MacBook-Air.local>
2024-01-16 12:43:03 -0600

    Testing faster STT

--- index.py
+++ index.py
@@ -59,7 +59,6 @@ torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 from faster_whisper import WhisperModel
 
 whisper = WhisperModel("large-v2")
-whisper.to(device)
 
 # whisper_model_id = "openai/whisper-large-v3"
 
